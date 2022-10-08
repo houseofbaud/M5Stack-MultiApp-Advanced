1dd0b861d7b4cfbeb270f41e6ee6209f6431fff3
baud <chance@houseofbaud.com>
2022-10-08 12:27:22 -0700

    initial import for ELMDuino/CANBUS support

---
+++ src/Apps/Tools/RPMGauge.cpp
@@ -0,0 +1,14 @@
+#include "BluetoothSerial.h"
+#include "ELMduino.h"
+
+
+BluetoothSerial SerialBT;
+#define ELM_PORT   SerialBT
+#define DEBUG_PORT Serial
+
+
+ELM327 myELM327;
+
+
+uint32_t rpm = 0;
+
---
+++ src/Apps/Tools/RPMGauge.h
@@ -0,0 +1,3 @@
+#pragma once
+#include "M5StackMod.h"
+
