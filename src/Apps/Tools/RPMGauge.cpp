89e1a5978c9e70dd14dbb5266dc434d3f6f5f5b5
baud <chance@houseofbaud.com>
2022-10-10 20:31:02 -0700

    Added connect logic for Bluetooth connection

--- src/Apps/Tools/RPMGauge.cpp
+++ src/Apps/Tools/RPMGauge.cpp
@@ -2,33 +2,73 @@
 
 void RPMGaugeClass::Run()
 {
-    M5m.update();
-    M5m.drawAppMenu(F("RPM GAUGE"), F("ESC"), F("A"), F("B"));
+  // Loop until the ESC button is pressed
+  while (!M5m.BtnA.wasPressed()) {
+    if (!ConnectELM())
+    {
+      M5m.Lcd.drawString(":(", 0, 40);
+      sleep(1000);
+    } else {
+      M5m.Lcd.drawString("OH SHIT IT WORKED", 0, 40);
+    }
+  }
+
+  return;
+}
+
+bool RPMGaugeClass::ConnectELM()
+{
+  DEBUG_PORT.println("Connecting to ODBII Module via Bluetooth...");
+
+  retryCount = 0;
+  ELM_PORT.begin("ESP32TEST", true); // Starts our module in Master mode with Broadcast name "ESP32TEST"
+
+  while (!ELMConnected)
+  {
+    if (retryCount < 3) {
+      ELMConnected = ELM_PORT.connect(name);
+    
+      if (!ELMConnected) {
+        ELMConnected = ELM_PORT.connected(5000);
+
+        if (!ELMConnected) {
+          DEBUG_PORT.println("Couldn't connect to OBD scanner - Phase 1");
+          DEBUG_PORT.printf("Retrying...%i\n", retryCount);
+        }
+      }
+    } else {
+      DEBUG_PORT.println("All connection attempts exhausted. Try again soon!");
+      ELM_PORT.disconnect();
+      return false;
+    }
+    retryCount++;
+  }
+  DEBUG_PORT.println("Bluetooth connection established.");
+
+  if (!myELM327.begin(ELM_PORT, true, 2000))
+  {
+    DEBUG_PORT.println("Couldn't connect to OBD scanner - Phase 2");
+    return false;
+  }
+
+  DEBUG_PORT.println("Connected to ELM327");
+  return true;
 }
 
 RPMGaugeClass::RPMGaugeClass()
 {
-  DEBUG_PORT.begin(115200);
-  //SerialBT.setPin("1234");
-  ELM_PORT.begin("MELODY", true);
-  
-  if (!ELM_PORT.connect("OBDII"))
-  {
-    DEBUG_PORT.println("Couldn't connect to OBD scanner - Phase 1");
-    return;
-  }
+  // Make sure we're not stalled by a previous failed run
+  ELMConnected = false;
+  //ELM_PORT.disconnect();
 
-  if (!myELM327.begin(ELM_PORT, true, 2000))
-  {
-    Serial.println("Couldn't connect to OBD scanner - Phase 2");
-    return;
-  }
-
-  Serial.println("Connected to ELM327");
+  M5m.drawAppMenu(F("RPM GAUGE"), F("ESC"), F("A"), F("B"));
+  M5m.update();
 }
 
 RPMGaugeClass::~RPMGaugeClass()
 {
+  ELM_PORT.disconnect();
+
 	M5m.Lcd.fillScreen(TFT_BLACK);
 	M5m.show();
 }
--- src/Apps/Tools/RPMGauge.h
+++ src/Apps/Tools/RPMGauge.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "M5StackMod.h"
 
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
 #define ELM_PORT   SerialBT
 #define DEBUG_PORT Serial
 
@@ -11,10 +15,22 @@ public:
     ~RPMGaugeClass();
 
     void Run();
+    bool ConnectELM();
+    bool IsELMConnected();
 
 private:
     BluetoothSerial SerialBT;
     ELM327 myELM327;
 
-    uint32_t rpm = 0;
+    bool ELMConnected;
+    uint8_t retryCount;
+    uint32_t rpm;
+
+    //String MACadd = "AA:BB:CC:11:22:33";
+    //uint8_t address[6]  = {0xAA, 0xBB, 0xCC, 0x11, 0x22, 0x33};
+
+    String name = "VHM-ble";  // the name of the Nonda ZUS adapter, ELM327 v1.5 firmware
+    const char *pin = "1234"; //<- standard pin would be provided by default
+
+
 };
